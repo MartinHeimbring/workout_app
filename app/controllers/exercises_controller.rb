411f7e1d62e1973259a1be9bd017d37b2a49eae9
Martin Heimbring <slovenien@hotmail.de>
2015-01-14 00:59:45 +0100

    create views for exercises + partials for workout_exercises

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,17 +1,44 @@
 class ExercisesController < ApplicationController
+  before_action :set_exercise, only: [:edit, :update, :destroy]
+
+  def index
+    @exercise = Exercise.new
+    @exercises = Exercise.all
+  end
+
+
   def create
-    @workout = Workout.find(params[:workout_id])
-    @exercise = @workout.exercises.create(exercise_params)
-    if @workout.save
-      redirect_to workout_path(@workout)
+    @exercise = Exercise.new(exercise_params)
+    if @exercise.save
+      redirect_to exercises_path
     else
       render 'new'
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @exercise.update(exercise_params)
+      redirect_to @exercises
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @exercise.destroy
+    redirect_to exercises_path
+  end
+
   private
 
     def exercise_params
-      params.require(:exercise).permit(:name, :sets, :reps)
+      params.require(:exercise).permit(:name)
+    end
+
+    def set_exercise
+      @exercise = Exercise.find(params[:id])
     end
 end
