491619a024b71e4280384756225e9ec4773b87aa
Martin Heimbring <martin.heimbring@gmail.com>
2015-01-19 13:46:55 +0100

    implement flash messages w/ bootstrap styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+
+  def bootstrap_class_for flash_type
+
+=begin
+    {
+        :success => 'alert-success',
+        :error => 'alert-danger',
+        :alert => 'alert-warning',
+        :notice => 'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+=end
+
+
+    case flash_type.to_sym
+      when :success
+        "alert-success"
+      when :error
+        "alert-error"
+      when :alert
+        "alert-block"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+
+  end
+
 end
