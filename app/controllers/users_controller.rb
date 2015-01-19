90d17993ba7e05f4927a24a44a6c4b116bccb47d
Martin Heimbring <martin.heimbring@gmail.com>
2015-01-19 14:33:28 +0100

    add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
   resources :workouts do
     resources :workout_exercises
   end
   resources :exercises
+  resources :users, except: [:index,:new,:create]
 
   root 'workouts#index'
 
