06f624c2235fee9f4fc1793a60ccadb59d6a949c
Martin Heimbring <martin.heimbring@gmail.com>
2015-01-19 18:25:15 +0100

    configure mail in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,27 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "https//my-workout-app.herokuapp.com" }
+  # ActionMailer Config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: ENV["SENDGRID_DOMAIN"]}
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'example.com',
+      :enable_starttls_auto => true
+  }
+
+
+  config.action_mailer.default :charset => "utf-8"
+
+
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
+
 
 end
