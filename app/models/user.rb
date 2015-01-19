861c4c88e8745a89f8caa9b75d8ac0707482d70b
Martin Heimbring <martin.heimbring@gmail.com>
2015-01-19 17:28:27 +0100

    add username to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,16 @@ class User < ActiveRecord::Base
 
   has_many :workouts
   has_many :exercises
+
+  validates :password,
+            presence: true,
+            confirmation: true,
+            length: { minimum: 5, maximum: 16 }
+
+  validates :username,
+            :uniqueness => {
+                :case_sensitive => false
+            }
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
